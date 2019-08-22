70f8ce210fc3e24de532c704dc3137235488d78e
hu53yin <huseyingurkan@gmail.com>
2019-08-22 03:04:23 +0300

    adding of mongodb models and auth modules

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,18 +1,53 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import React from 'react';
+import { GraphQLClient } from 'graphql-request';
+import { GoogleLogin } from 'react-google-login';
+import { withStyles } from '@material-ui/core/styles';
 // import Typography from "@material-ui/core/Typography";
 
+const ME_QUERY = `
+{
+  me {
+    _id
+    name
+    email
+    picture
+  }
+}`;
+
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onFailure = err => {
+    console.log({ err });
+  };
+
+  const onSuccess = async googleUser => {
+    const idToken = googleUser.getAuthResponse().id_token;
+
+    const client = new GraphQLClient('http://localhost:4000/graphql', {
+      headers: { authorization: idToken }
+    });
+
+    const data = await client.request(ME_QUERY);
+
+    console.log({ data });
+  };
+
+  return (
+    <GoogleLogin
+      clientId='728470308409-118cqukl0377a70p1uattaagpqcag1t2.apps.googleusercontent.com'
+      onSuccess={onSuccess}
+      onFailure={onFailure}
+      isSignedIn={true}
+    />
+  );
 };
 
 const styles = {
   root: {
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center"
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center'
   }
 };
 
