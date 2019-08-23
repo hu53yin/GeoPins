8dde651c0e0342f8f9c9b077e8ac8889c106061b
Hüseyin GÜRKAN (Hesap ve Muhasebe Uygulamaları 1) <hgurkan@fintek.local>
2019-08-23 17:53:14 +0300

    Blog component development has been started

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
-import ReactMapGL, { NavigationControl } from 'react-map-gl';
+import React, { useState, useEffect, useContext } from 'react';
+import ReactMapGL, { NavigationControl, Marker } from 'react-map-gl';
 import { withStyles } from '@material-ui/core/styles';
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+
+import PinIcon from './PinIcon';
+import Blog from './Blog';
+import Context from '../context';
 
 const INITIAL_VIEWPORT = {
   latitude: 37.7577,
@@ -12,7 +16,40 @@ const INITIAL_VIEWPORT = {
 };
 
 const Map = ({ classes }) => {
+  const { state, dispatch } = useContext(Context);
   const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+  const [userPosition, setUserPosition] = useState(null);
+  useEffect(() => {
+    getUserPosition();
+  }, []);
+
+  const getUserPosition = () => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        setViewport({ ...viewport, latitude, longitude });
+        setUserPosition({ latitude, longitude });
+      });
+    }
+  };
+
+  const handleMapClick = ({ lngLat, leftButton }) => {
+    if (!leftButton) return;
+
+    if (!state.draft) {
+      dispatch({ type: 'CREATE_DRAFT' });
+    }
+
+    const [longitude, latitude] = lngLat;
+
+    dispatch({
+      type: 'UPDATE_DRAFT_LOCATION',
+      payload: {
+        longitude,
+        latitude
+      }
+    });
+  };
 
   return (
     <div className={classes.root}>
@@ -22,6 +59,7 @@ const Map = ({ classes }) => {
         mapStyle='mapbox://styles/mapbox/streets-v9'
         mapboxApiAccessToken='TOKEN_INFO_HERE'
         onViewportChange={newViewport => setViewport(newViewport)}
+        onClick={handleMapClick}
         {...viewport}
       >
         {/* Navigation Control */}
@@ -30,7 +68,33 @@ const Map = ({ classes }) => {
             onViewportChange={newViewport => setViewport(newViewport)}
           />
         </div>
+        {/* Pin for User's Current Position */}
+        {userPosition && (
+          <Marker
+            latitude={userPosition.latitude}
+            longitude={userPosition.longitude}
+            offsetLeft={-19}
+            offsetTop={-37}
+          >
+            <PinIcon size={40} color='red' />
+          </Marker>
+        )}
+
+        {/* Draft Pin */}
+        {state.draft && (
+          <Marker
+            latitude={state.draft.latitude}
+            longitude={state.draft.longitude}
+            offsetLeft={-19}
+            offsetTop={-37}
+          >
+            <PinIcon size={40} color='hotpink' />
+          </Marker>
+        )}
       </ReactMapGL>
+
+      {/* Blog area to add Pin Content */}
+      <Blog />
     </div>
   );
 };
